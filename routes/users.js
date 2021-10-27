609f2a8441c6486264b91fd61fb08a802d305c84
huytunguyenn <49774218+huytunguyenn@users.noreply.github.com>
2021-10-27 21:17:00 +0700

    make it better

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const classesService = require('../components/classes/classesService')
-
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-  const classes = await classesService.list()
-  res.send(classes);
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
